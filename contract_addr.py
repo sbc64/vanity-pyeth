06a2d2b1326a22141224c74111785400d094c8ff
Sebastian Bolanos <sebasheston@gmail.com>
2018-02-25 17:38:02 -0800

    Gives you the first 3 contract address assuming nonce 0

--- contract_addr.py
+++ contract_addr.py
@@ -1,67 +1,49 @@
 #!/usr/bin/env python3
 
-from web3 import Web3, HTTPProvider
-import json
 import os
-from sh import seth
-import rlp
-import hashlib
-import json
 import ast
-from ethereum.utils import mk_contract_address, sha3, normalize_address, encode_hex
+from ethereum.utils import (
+        mk_contract_address,
+        sha3,
+        normalize_address,
+        encode_hex,
+        privtoaddr,
+        checksum_encode
+    )
 
-
-C = {'ROPSTEN': os.environ['ROPSTEN'], 'MAIN': os.environ['MAINNET'] ,'TEST': 'HTTP://127.0.0.1:7545'}
-
-
-
-def addr_port(var):
-    s = var.split(':')
-    addr = s[0]+':'+s[1]
-    port = s[-1]
-    return addr, port
-
-
-def build_command(inputs):
-
-    for mem in inputs:
-        if type(mem) == type(list):
-            print ()
+def generate_pk():
     
-            c_list.append()
-            
-        if type(mem) == type('s'):
-            print ()
+    pk = sha3(os.urandom(4096))
 
-    return 
+    return encode_hex(pk)
+
 
 if __name__=='__main__':
 
+    # VARS
+    to_generate = 10
+    contracts_to_gen = 3
 
 
-    # Use vanity-eth npm for address generation
-    dict_list = []
-    with open('addresses', 'r') as addresses:
-        for line in addresses:
-            
-            dict_list.append(ast.literal_eval(line))
-            a = dict_list[-1]['address']
-            contracts = []
+    keys_dict_list = []
+    for x in range(to_generate):
+        keys_dict_list.append({"pk": generate_pk()})
 
-            for i in range(3):
-                
-                addr_ = str(encode_hex(mk_contract_address(a, i)))
-                addr_ = addr_[1:]
-                contracts.append("0x"+addr_.replace("\'",""))
+    for member in keys_dict_list:
+        a = checksum_encode(privtoaddr(member['pk']))
+        member['pa'] = a
 
-            
-            dict_list[-1]['contracts'] = contracts
-        
-        with open('output', 'w+') as out:
-           
-            for x in dict_list:
-               
-                out.write(str(x))
-                out.write("\n\n")
+    
+    for member in keys_dict_list:
+        contracts = []
+        for i in range(contracts_to_gen):
+            c_addr_ = str(encode_hex(mk_contract_address(member['pa'], i)))
+            contracts.append("0x" + c_addr_.replace("\'",""))
 
-        
+        member['contracts'] = contracts 
+
+    
+    for m in keys_dict_list:
+        print (m)
+        print ()
+
