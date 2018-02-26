24ea7f37bb610f8256b702d4a6e02154f6f88b1f
Sebastian Bolanos <sebasheston@gmail.com>
2018-02-25 20:19:38 -0800

    Working alpha of private key multi tool

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://github.com/MyEtherWallet/VanityEth
+
+if you want to generate private keys and its respective contract address use the following command:
+    ./contract_addr.py -k -q 10 | awk '{print }' | sed 's|,||g' | xargs -I {} ./contract_addr.py -pk {} -q 3
--- contract_addr.py
+++ contract_addr.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import time
 import os
 import ast
+import argparse
 from ethereum.utils import (
         mk_contract_address,
         sha3,
@@ -12,38 +14,134 @@ from ethereum.utils import (
     )
 
 def generate_pk():
-    
     pk = sha3(os.urandom(4096))
-
     return encode_hex(pk)
 
+# generates a single private and public dict
+def private_and_pub():
+
+    pk = generate_pk()
+    return {'pk': pk,
+            'pa': checksum_encode(privtoaddr(pk))}
+    
+def only_keys(quantity):
+
+    for x in range(to_generate):
+        print (private_and_pub()) 
+        print ()
+
+def check_hex_str(string):
+
+    # can't get string.hexdigits to work
+    if not (all(c in '0123456789abcdefABCDEF' for c in string)):
+        raise("Not a hexadecimal string")
+
+
+def vanity_contracts(quantity_of_contracts, vanity):
+   
+    check_hex_str(vanity)
+    nonce_count = 0
+    pk = generate_pk()
+
+    while True:
+            
+        contract = generate_contracts(1, pk, nonce_count)[0]
+            
+        if vanity == contract[2:2+len(vanity)]:
+            nonce_count += 1
+            if nonce_count == quantity_of_contracts:
+                break
+
+        else:
+            pk = generate_pk()
+            nonce_count = 0
+
+    return (pk)
+
+# Generates the contracts address from given private key
+def generate_contracts(quantity_per_key, private_key, nonce=0):
+    
+    pa = checksum_encode(privtoaddr(private_key))
+    
+    contracts = []
+    for i in range(quantity_per_key):
+        c_addr_ = str(encode_hex(mk_contract_address(pa, i + nonce)))
+        contracts.append("0x" + c_addr_.replace("\'",""))
+
+    return contracts
 
 if __name__=='__main__':
 
+
+    parser = argparse.ArgumentParser(
+            description='Ethereum contract address generator, can also generate private keys')
+
+    parser.add_argument('-pk',
+                        '--private-key',
+                        dest="SUPPLIED_KEYS",
+                        default='',
+                        nargs='*',
+                        type=str,
+                        help="private key to use for generating contract addresses"
+                       )
+
+    parser.add_argument('-n',
+                        '--nonce',
+                        dest="NONCE",
+                        type=int,
+                        default=0,
+                        help="nonce of the privided private key"
+                        )
+
+    parser.add_argument('-q',
+                        '--quantity',
+                        dest="C_TO_GEN",
+                        type=int,
+                        default=1,
+                        help="quantity of contracts to generate or quantiy of keys if -k flag is used"
+                        )
+
+    parser.add_argument('-V',
+                        '--vanity',
+                        dest="VANITY_STR",
+                        type=str,
+                        default='',
+                        help="""vanity chars in the contracts addresses.\n
+                              WARNING: more than four chars will increase duration by A LOT
+
+                              """
+                        )
+
+    parser.add_argument('-k',
+                        '--keys',
+                        dest="ONLY_KEYS",
+                        action='store_true'
+                        )
+
+
+    try:
+        args = parser.parse_args()
+    except:
+        exit(2)
+
     # VARS
-    to_generate = 10
-    contracts_to_gen = 3
+    to_generate = args.C_TO_GEN
+    contracts_to_gen = args.C_TO_GEN
+    nonce = args.NONCE
 
-
-    keys_dict_list = []
-    for x in range(to_generate):
-        keys_dict_list.append({"pk": generate_pk()})
-
-    for member in keys_dict_list:
-        a = checksum_encode(privtoaddr(member['pk']))
-        member['pa'] = a
-
+    if args.ONLY_KEYS:
+        only_keys(quantity=args.C_TO_GEN)
+        exit(0)
     
-    for member in keys_dict_list:
-        contracts = []
-        for i in range(contracts_to_gen):
-            c_addr_ = str(encode_hex(mk_contract_address(member['pa'], i)))
-            contracts.append("0x" + c_addr_.replace("\'",""))
+    if args.VANITY_STR:
+        pk = vanity_contracts(args.C_TO_GEN, args.VANITY_STR)
+        c = generate_contracts(args.C_TO_GEN, pk)
+        print ("pk: {}\ncontracts: {}".format(pk, c))
+        exit(0)
 
-        member['contracts'] = contracts 
-
-    
-    for m in keys_dict_list:
-        print (m)
-        print ()
-
+    if args.SUPPLIED_KEYS:
+        for pk in args.SUPPLIED_KEYS:
+            contracts = generate_contracts(args.C_TO_GEN, pk)
+            
+            print ('\npk: {} \ncontracts: {}\n\n'.format(pk, contracts))
+        exit(0)
